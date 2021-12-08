0ec1984dc3f2e19163dd437237a64ba1b6820044
like-ycy <15210198546@163.com>
2021-12-08 21:36:49 +0800

    roken校验和刷新接口

--- hippoapi/hippoapi/apps/users/urls.py
+++ hippoapi/hippoapi/apps/users/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from rest_framework_jwt.views import verify_jwt_token, refresh_jwt_token
+from rest_framework_jwt.views import refresh_jwt_token
 
 from . import views
 
 urlpatterns = [
     path("login/", views.LoginAPIView.as_view(), name="login"),
-    path('verify/', verify_jwt_token),  # 这是只是校验token有效性
-    path(r'verify_and_refresh/', refresh_jwt_token),  # 校验并生成新的token
+    # path('verify/', verify_jwt_token),  # 这是只是校验token有效性
+    path(r'refresh/', refresh_jwt_token),  # 校验并生成新的token
 ]
