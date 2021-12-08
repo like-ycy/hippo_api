b2efd45ed040bebbb3863b81ecccbb26b737cc5c
like-ycy <15210198546@163.com>
2021-12-08 18:46:26 +0800

    为后边扩展功能提前实现接口

--- hippoapi/hippoapi/apps/users/urls.py
+++ hippoapi/hippoapi/apps/users/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from rest_framework_jwt.views import verify_jwt_token, refresh_jwt_token
 
 from . import views
 
 urlpatterns = [
     path("login/", views.LoginAPIView.as_view(), name="login"),
+    path('verify/', verify_jwt_token),  # 这是只是校验token有效性
+    path(r'verify_and_refresh/', refresh_jwt_token),  # 校验并生成新的token
 ]
