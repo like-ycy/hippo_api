27b25891b7aee3684a9c75748d4011f1954c54bf
like-ycy <15210198546@163.com>
2021-12-10 18:41:41 +0800

    console功能的实现

---
+++ hippoapi/hippoapi/apps/consumer/routing.py
@@ -0,0 +1,14 @@
+from channels.routing import URLRouter
+from django.urls import path
+
+from .consumers import *
+from .middleware import AuthMiddleware
+
+# 由于我们可能会对websocket请求进行一些验证或者身份认证，所以我们在consumer应用下面在创建一个middleware文件，里面可以配置一些认证规则
+ws_router = AuthMiddleware(
+    URLRouter([
+        # path('ws/exec/<str:token>/', ExecConsumer),
+        path('ws/ssh/<int:id>/', SSHConsumer),
+        # path('ws/release/<int:id>/', ReleaseConsumer),
+    ])
+)
