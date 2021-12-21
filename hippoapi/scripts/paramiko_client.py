c4a77d93bb8dfb37e3add0009a6fbfa7ae156011
like-ycy <15210198546@163.com>
2021-12-21 16:19:39 +0800

    测试脚本：使用paramiko测试公钥连接远程主机

--- hippoapi/scripts/paramiko_client.py
+++ hippoapi/scripts/paramiko_client.py
@@ -1,7 +1,4 @@
-from io import StringIO
-
 import paramiko
-from paramiko.rsakey import RSAKey
 from paramiko.ssh_exception import AuthenticationException
 
 """
@@ -13,11 +10,11 @@ ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 try:
     # 1、密码连接
-    # ssh.connect(hostname='192.168.174.23', username='root', password='123.com')
+    ssh.connect(hostname='192.168.174.24', username='root', password='123.com')
     # 2、秘钥连接
-    pkey = RSAKey.from_private_key(StringIO(pkey))
-    ssh.connect(hostname='192.168.174.23', username='root', pkey=pkey)
-    stdin, stdout, stderr = ssh.exec_command('ls -al')
+    # pkey = RSAKey.from_private_key(StringIO(pkey))
+    # ssh.connect(hostname='192.168.174.23', username='root', pkey=pkey)
+    stdin, stdout, stderr = ssh.exec_command('mkdir -p /mnt/test && ls /mnt/')
     result = stdout.read()
     print(result.decode())
     ssh.close()
